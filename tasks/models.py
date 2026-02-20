4e74d9b1b95ba0466096fe6633cb4b42db773ecc
Stavros <n.stavros@outlook.de>
2026-02-20 18:07:54 +0100

    Add task status with choices and update CRUD

--- tasks/models.py
+++ tasks/models.py
@@ -2,8 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
+    STATUS_CHOICES = [
+        ("open", "Open"),
+        ("done", "Done"),
+    ]
     title = models.CharField(max_length=255)
-
+    status = models.CharField(max_length=255, choices=STATUS_CHOICES, default="open")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
 
     def __str__(self):
