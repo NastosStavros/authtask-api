016e8a38ac4a24289ef27ed57a4b89d356657db3
Stavros <n.stavros@outlook.de>
2026-02-28 17:26:12 +0100

    Add UserSerializer + nested user in TaskSerializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import Task
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username"]
 
 class TaskSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    
     class Meta:
         model = Task
         fields = "__all__"
+
